5ac689ebd85c3a56feff50773dd02a7cc7aefeac
Rustam <rustamzakirov@mail.ru>
2023-02-18 20:49:18 +0300

    first complete

--- 001/Program.cs
+++ 001/Program.cs
@@ -1,2 +1,34 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+
+
+void InputMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = Math.Round((new Random().Next(-99,100) + new Random().NextDouble()), 2);
+    }
+}
+
+void PrintMatrix(double[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            Console.Write($"{matrix[i, j]} \t");
+        Console.WriteLine();
+    }
+}
+
+Console.Clear();
+Console.Write("Введите размер массива: ");
+double[] size = Console.ReadLine().Split().Select(x => double.Parse(x)).ToArray();
+double[,] matrix = new double[(int)size[0], (int)size[1]];
+InputMatrix(matrix);
+PrintMatrix(matrix);
+
